511258980287df2078e0f3b205a566b9a20e74c5
Ali Koçman <34556282+Eddith@users.noreply.github.com>
2022-03-06 13:59:44 +0300

    express and graphql start create and update added

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,36 @@
+import 'reflect-metadata';
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "My first post", createdAt: new Date(), updatedAt: new Date() });
-  await orm.em.persistAndFlush(post);
-  console.log("----------sql 2----------");
-  await orm.em.nativeInsert(post, { title: "My second post2" });
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false
+    }),
+    context: () => ({ em: orm.em })
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 }
 
 main().catch((err) => {
   console.error(err);
 });  
-
-// https://www.youtube.com/watch?v=I6ypD7qv3Z8
